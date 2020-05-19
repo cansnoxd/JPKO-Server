0f0e79a57735fb25b952d98dcdcacfeb72346565
can <62135319+cansnoxd@users.noreply.github.com>
2020-05-19 04:40:57 +0300

    Add files via upload

---
+++ Server Management Readme.txt
@@ -0,0 +1,15 @@
+Look at ServerManagementGuide.png
+
+-Folder names:
+AIServer
+Aujard
+Billing server
+Ebenezer
+Login server
+
+-Exe names:
+AI Server.exe
+Aujard.exe
+billing-server.exe
+Ebenezer.exe
+login-server.exe
---
+++ Server Management.bat
@@ -0,0 +1,60 @@
+@echo off
+color 0a
+set mtext=What you want to do?: 
+
+:Main
+echo.
+echo.
+echo.
+echo.
+echo.
+echo 	1) Start Server
+echo.
+echo 	2) Close Server
+echo.
+echo 	3) Clean Log Files
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p uip=%mtext%
+if %uip%==1 goto:Start
+if %uip%==2 goto:Stop
+if %uip%==3 goto:Clean
+if not %uip%==1 if not %uip%==2 if not %uip%==3 goto:TryAgain
+exit
+
+:Start
+cd AIServer && start /min "" "AI Server.exe"
+cd ..\Ebenezer && start /min Ebenezer.exe
+cd ..\Billing server && start /min billing-server.exe
+cd ..\Aujard && start /min Aujard.exe
+cd ..\Login server && start /min login-server.exe && cd ..
+cls
+set mtext=Do you want to do something else?: 
+goto:Main
+
+:Stop
+wmic process where name="login-server.exe" delete >nul
+wmic process where name="Aujard.exe" delete >nul
+wmic process where name="billing-server.exe" delete >nul
+wmic process where name="Ebenezer.exe" delete >nul
+wmic process where name="AI Server.exe" delete >nul
+cls
+set mtext=Do you want to do something else?: 
+goto:Main
+
+:Clean
+cd AIServer && del *.txt *.log >nul 2>&1
+cd ..\Ebenezer && for %%i in (*.txt *.log) do if not %%i==BlockWord.txt if not %%i==HackToolList.txt if not %%i==Notice.txt del %%i >nul 2>&1
+cd ..\Aujard && del *.txt *.log >nul 2>&1
+cd ..\Login server && del *.log >nul 2>&1 && cd ..
+cls
+set mtext=Do you want to do something else?: 
+goto:Main
+
+:TryAgain
+cls
+set mtext=Choose one of the above: 
+goto:Main
