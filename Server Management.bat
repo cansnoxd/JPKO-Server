fdb2d53dd7eb3277cf32b335db5fdb425d0c3a05
can <62135319+cansnoxd@users.noreply.github.com>
2020-06-15 00:45:28 +0300

    Add files via upload

--- Server Management.bat
+++ Server Management.bat
@@ -12,7 +12,9 @@ echo 	1) Start Server
 echo.
 echo 	2) Close Server
 echo.
-echo 	3) Clean Log Files
+echo 	3) Restart Server
+echo.
+echo 	4) Clean Log Files
 echo.
 echo.
 echo.
@@ -20,9 +22,10 @@ echo.
 echo.
 set /p uip=%mtext%
 if %uip%==1 goto:Start
-if %uip%==2 goto:Stop
-if %uip%==3 goto:Clean
-if not %uip%==1 if not %uip%==2 if not %uip%==3 goto:TryAgain
+if %uip%==2 goto:Close
+if %uip%==3 goto:Restart
+if %uip%==4 goto:Clean
+if not %uip%==1 if not %uip%==2 if not %uip%==3 if not %uip%==4 goto:TryAgain
 exit
 
 :Start
@@ -35,12 +38,27 @@ cls
 set mtext=Do you want to do something else?: 
 goto:Main
 
-:Stop
+:Close
 wmic process where name="login-server.exe" delete >nul
 wmic process where name="Aujard.exe" delete >nul
 wmic process where name="billing-server.exe" delete >nul
 wmic process where name="Ebenezer.exe" delete >nul
 wmic process where name="AI Server.exe" delete >nul
+cls
+set mtext=Do you want to do something else?: 
+goto:Main
+
+:Restart
+wmic process where name="login-server.exe" delete >nul
+wmic process where name="Aujard.exe" delete >nul
+wmic process where name="billing-server.exe" delete >nul
+wmic process where name="Ebenezer.exe" delete >nul
+wmic process where name="AI Server.exe" delete >nul
+cd AIServer && start /min "" "AI Server.exe"
+cd ..\Ebenezer && start /min Ebenezer.exe
+cd ..\Billing server && start /min billing-server.exe
+cd ..\Aujard && start /min Aujard.exe
+cd ..\Login server && start /min login-server.exe && cd ..
 cls
 set mtext=Do you want to do something else?: 
 goto:Main
