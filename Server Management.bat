c8ade76ea36dce6973f27e45764149ff12df55c3
can <62135319+cansnoxd@users.noreply.github.com>
2020-07-30 17:02:19 +0300

    Add files via upload

--- Server Management.bat
+++ Server Management.bat
@@ -10,16 +10,18 @@ echo.
 echo.
 echo    1) Start Server
 echo.
-echo    2) Close Server
+echo    2) Close Everything
 echo.
 echo    3) Restart Server
 echo.
 echo    4) Clean Log Files
 echo.
-echo    5) Open Aujard (Windows 8 Fix)
+echo    5) Open Aujard Again (Windows 8.1 Fix)
 echo.
+echo    6) Open Scheduler
 echo.
-echo.
+echo.   
+echo.	
 echo.
 echo.
 set /p uip=%mtext%
@@ -28,7 +30,8 @@ if %uip%==2 goto:Close
 if %uip%==3 goto:Restart
 if %uip%==4 goto:Clean
 if %uip%==5 goto:Aujard
-if not %uip%==1 if not %uip%==2 if not %uip%==3 if not %uip%==4 if not %uip%==5 goto:TryAgain
+if %uip%==6 goto:Scheduler
+if not %uip%==1 if not %uip%==2 if not %uip%==3 if not %uip%==4 if not %uip%==5 if not %uip%==6 goto:TryAgain
 exit
  
 :Start
@@ -36,6 +39,7 @@ cd Login server && start /min login-server.exe
 cd ..\AIServer && start /min "" "AI Server.exe"
 cd ..\Aujard && start /min Aujard.exe
 cd ..\Ebenezer && start /min Ebenezer.exe
+cd ..\Ebenezer && start /min DupeScanner.exe
 cd ..\Billing server && start /min billing-server.exe && cd ..
 cls
 set mtext=Do you want to do something else?:
@@ -47,6 +51,8 @@ wmic process where name="AI Server.exe" delete >nul
 wmic process where name="Aujard.exe" delete >nul
 wmic process where name="Ebenezer.exe" delete >nul
 wmic process where name="billing-server.exe" delete >nul
+wmic process where name="DupeScanner.exe" delete >nul
+wmic process where name="KOScheduler.exe" delete >nul
 cls
 set mtext=Do you want to do something else?:
 goto:Main
@@ -56,11 +62,13 @@ wmic process where name="login-server.exe" delete >nul
 wmic process where name="AI Server.exe" delete >nul
 wmic process where name="Aujard.exe" delete >nul
 wmic process where name="Ebenezer.exe" delete >nul
+wmic process where name="DupeScanner.exe" delete >nul
 wmic process where name="billing-server.exe" delete >nul
 cd Login server && start /min login-server.exe
 cd ..\AIServer && start /min "" "AI Server.exe"
 cd ..\Aujard && start /min Aujard.exe
 cd ..\Ebenezer && start /min Ebenezer.exe
+cd ..\Ebenezer && start /min DupeScanner.exe
 cd ..\Billing server && start /min billing-server.exe && cd ..
 cls
 set mtext=Do you want to do something else?:
@@ -80,7 +88,13 @@ cd Aujard && start /min Aujard.exe && cd ..
 cls
 set mtext=Do you want to do something else?:
 goto:Main
- 
+
+:Scheduler
+cd Ebenezer && start /min KOScheduler.exe && cd ..
+cls
+set mtext=Do you want to do something else?:
+goto:Main
+
 :TryAgain
 cls
 set mtext=Choose one of the above:
