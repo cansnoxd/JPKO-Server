2b39f9eb48c67f3ca033bcd16c451eba76a47d94
can <62135319+cansnoxd@users.noreply.github.com>
2020-08-02 03:13:45 +0300

    Rename Create user with without premium.sql to Create User (Non Premium).sql

---
+++ Scripts/User Scripts/Create User (Non Premium).sql
@@ -0,0 +1,4 @@
+﻿--simply creates user without premium--
+--(acc name) (acc password)(empty) (empty) (royality 1) (- premium days)--
+--Can_SnoxD--
+insert into TB_USER values ('testo','testo','','','1','2019-03-7')
