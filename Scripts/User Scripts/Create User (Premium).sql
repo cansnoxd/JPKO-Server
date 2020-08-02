bd844b182e506f0a05811732f5c921c111181fce
can <62135319+cansnoxd@users.noreply.github.com>
2020-08-02 03:13:22 +0300

    Rename Create user with premium.sql to Create User (Premium).sql

---
+++ Scripts/User Scripts/Create User (Premium).sql
@@ -0,0 +1,4 @@
+﻿--simply creates user with premium--
+--(acc name) (acc password)(empty) (empty) (royality 1) (+ premium days)--
+--Can_SnoxD--
+insert into TB_USER values ('testo','testo','','','1','2020-03-7')
