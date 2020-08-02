3cbefa32d43309e372554807afa72e12efc86884
can <62135319+cansnoxd@users.noreply.github.com>
2020-08-02 03:14:06 +0300

    Rename Give premium.sql to Give Premium.sql

---
+++ Scripts/User Scripts/Give Premium.sql
@@ -0,0 +1,4 @@
+﻿--EXEC (premium table) (account name) (+ premium days)--
+--30 is premium days you can use this code many times as you want even 1 year premium--
+--Can_SnoxD--
+EXEC GIVE_PREMIUM 'testo', '', 30
